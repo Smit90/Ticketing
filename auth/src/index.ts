54f517c1ed27467a0448de063285220c1dd8ddb2
Smit90 <45687440+Smit90@users.noreply.github.com>
2023-02-11 10:43:53 +0530

    Auth routes, middlewares and error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,25 @@
 import express from 'express'
+import 'express-async-errors'
 import { json } from 'body-parser'
+import { currentUserRouter, signinRouter, signoutRouter, singupRouter } from './routes/index'
+import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/index'
 
 const app = express()
-
 app.use(json())
+
+// routes
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(singupRouter)
+app.use(signoutRouter)
+
+app.all('*', async () => {
+    throw new NotFoundError()
+})
+
+// error handler
+app.use(errorHandler)
 
 app.listen(3000, () => {
     console.log("Auth Service running on port 3000!")
---
+++ auth/src/routes/index.ts
@@ -0,0 +1,11 @@
+import { currentUserRouter } from './current-user'
+import { signinRouter } from './sigin'
+import { signoutRouter } from './signout'
+import { singupRouter } from './singup'
+
+export {
+    currentUserRouter,
+    signinRouter,
+    signoutRouter,
+    singupRouter
+}
